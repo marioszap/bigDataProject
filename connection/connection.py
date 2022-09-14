352749bca4410dcb3335f224e6e342c5e745565d
marioszap <66917833+marioszap@users.noreply.github.com>
2022-09-14 12:47:35 +0300

    Add files via upload

--- connection/connection.py
+++ connection/connection.py
@@ -6,10 +6,10 @@ cloud_config= {
 }
 auth_provider = PlainTextAuthProvider("FKDIkqtNHPhiyxCZFNdzeKMS", "rAleUiXfRycLJkw,c,7c30MDBSp3aqeTZlIuWQlQDGTyNvg---GnNFh9bOqfvYWpdoWZNmtWU3SDlffApXTawewwOCgvAICn-xyTkMb7pHKY.HtN7tLfLBDhDw+57nf6")
 cluster = Cluster(cloud = cloud_config, auth_provider = auth_provider)
-session = cluster.connect()
-
+session = cluster.connect('mm')
 row = session.execute("select release_version from system.local").one()
 if row:
     print(row[0])
 else:
     print("An error occurred.")
+#session.execute('--client-request-timeout = None;')
